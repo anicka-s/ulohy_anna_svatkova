33364100d5fa1ca6110881c10d39aa83a0096194
anicka-s <57623721+anicka-s@users.noreply.github.com>
2021-01-13 14:57:34 +0100

    Vytvoření seznamu znaků (bez mezer!)

--- #4_cetnost.py
+++ #4_cetnost.py
@@ -9,7 +9,11 @@ def nacteni(nazev_souboru):
 
 # >> PROGRAM <<
 text = nacteni('vstupni_soubor.txt')
-
+seznam_prvku = text.split()
+seznam_znaku = []
+for prvek in seznam_prvku:
+    for znak in prvek:
+        seznam_znaku.append(znak)
 
 # TODO: Ulozeni vystupu do .txt souboru
-print(text)
+print(seznam_znaku)
