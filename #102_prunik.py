7c67a1c219224936892952b51db04e5cb415180a
anicka-s <57623721+anicka-s@users.noreply.github.com>
2021-02-02 01:42:32 +0100

    Část ošetření vstupů
    - nadbytečné mezery
    - nepodporované znaky
    - když neobsahuje číslice

--- #102_prunik.py
+++ #102_prunik.py
@@ -1,10 +1,37 @@
+import os
+import re
+
 # >> FUNKCE <<
 def nacteni(nazev_souboru):
     # TODO: Docstring
-    # TODO: Osetreni chybnych vstupu - souborove chyby, vstupy nejsou cisla...
-    with open(nazev_souboru, mode='r', encoding='utf-8') as data:
-        hodnoty = data.read()
-    return(hodnoty)
+    if os.path.exists(nazev_souboru) and os.path.getsize(nazev_souboru) and os.access(nazev_souboru, os.R_OK) > 0:
+        try: # mozna nebude potreba
+            with open(nazev_souboru, mode='r', encoding='utf-8') as data:
+                # TODO: osetreni vstupy obsahuje jen '0123456789. '; musi obsahovat cislice
+
+                hodnoty = data.read()
+                doplnek_ok_znaky = r"[^-0-9\. ]"
+                cislice = r"[0-9]+"
+                if '  ' in hodnoty:
+                    print('Neplatné oddělení mezerami - nadbytek mezer.')
+                    exit()
+                elif re.match(doplnek_ok_znaky, hodnoty):
+                    print('Neplatný vstup - obsahuje nepodporované znaky.\n(Podporované jsou 0-9, minus, des. tečka, mezera.)')
+                    exit()
+                elif not re.search(cislice, hodnoty):
+                    print('Neplatný vstup - neobsahuje číselné hodnoty.')
+                    exit()
+                else:
+                    print("zatím to prošlo, ale není ještě validován formát")
+                    return(hodnoty)
+                    
+        except ValueError:
+            print("Vstupní soubor není v platném formátu.")
+            exit()
+    else:
+        print("Vstupní soubor chybí, je prázdný, nebo k němu není povolen přístup.")
+        exit()
+
 
 def prunik(seznam_hodnot1,seznam_hodnot2):
     # TODO: Docstring
@@ -15,23 +42,21 @@ def prunik(seznam_hodnot1,seznam_hodnot2):
                 seznam_prunik.append(cislo)
     return(seznam_prunik)
 
-# vyjimky:
-# osetreni souboru: neexistujici/prazdny/nepristupny vstupni soubor
-# neoddeleno mezerami: if neobsahuje ' '
-# des. carka nebo oddeleno carkami: if cislo obsahuje carku, nahradit teckou (-> rozlozi cyklus nize)
-# nebo vseobecne: pokud obsahuje soubor cokoliv jineho, nez cislice, tecky nebo mezery -> ukonceni (chyba formatu)
-# obecne osetreni erroru
+# TODO: zamezeni vstupu obsahujicich napr. '..4.' ci '.4...4..' atd. -> obecne, bude vyhozeno
+# v ramci cyklu lines 39 a 40, kde to zjisti, ze format neni float ale str
+# TODO: a osetreni !-' tam kde nema bejt - v cyklu struktura regex
+
 
 
 # >> PROGRAM <<
 hodnoty1 = nacteni('hodnoty1.txt')
 hodnoty2 = nacteni('hodnoty2.txt')
-# TODO: Vstup oddeleni carkou osetrit
 seznam1 = [float(hodnota) for hodnota in hodnoty1.split(' ')]
 seznam2 = [float(hodnota) for hodnota in hodnoty2.split(' ')]
 
 prunik_posloupnosti = prunik(seznam1,seznam2)
 prunik_posloupnosti.sort()
 
+print(prunik_posloupnosti)
 with open('prunik_posloupnosti.txt', mode='w', encoding='utf-8') as output:   # w jako write >>> mode='a' -> append, tedy připojit na konec souboru
     print(prunik_posloupnosti, file=output)
